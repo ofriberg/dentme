4ba71e7d885a9fd8d40a1f14dae3338c49dc52af
Oscar Friberg <oscfriberg@gmail.com>
2026-01-30 19:29:18 +0100

    delete user test

--- tests/userService.test.ts
+++ tests/userService.test.ts
@@ -19,26 +19,37 @@ describe('userService', () => {
     expect(user.email).toBe("anders@andersson.se");
   });
 
-  it("throws an error on duplicate emails", () => {
+  it("deletes a user", () => {
+    const user = userService.create({
+      firstName: "Anders",
+      lastName: "Andersson",
+      email: "anders@andersson.se"
+    });
+
+    expect(userService.list().length).toBe(1);
+
+    expect(() => userService.deleteById(user.id)).not.toThrow();
+
+    expect(userService.list().length).toBe(0);
+
+  });
+
+  it("throws a 409 error on duplicate emails", () => {
     userService.create({
       firstName: "Anders",
       lastName: "Andersson",
       email: "anders@andersson.se"
     });
 
-    expect(() => {
-      userService.create({
-        firstName: "Anders",
-        lastName: "Anders",
-        email: "anders@andersson.se"
-      })
-    }).toThrowError(HttpError);
-
     try {
-      userService.create({ firstName: "C", lastName: "D", email: "anders@andersson.se" });
+      userService.create({
+        firstName: "C",
+        lastName: "D",
+        email: "anders@andersson.se"
+      });
       throw new Error("Expected a HttpError, but wasnt thrown");
     }
-    catch (err) {
+    catch (err: unknown) {
       expect(err).toBeInstanceOf(HttpError);
       const error = err as HttpError;
       expect(error.status).toBe(409);
@@ -46,8 +57,17 @@ describe('userService', () => {
     }
   });
 
-  it("throws 404 when deleting a muissing user", () => {
-    expect(() => userService.deleteById("does-not-exist")).toThrowError(HttpError)
+  it("throws 404 when deleting a missing user", () => {
+    try {
+      userService.deleteById("does-not-exist");
+      throw new Error("Expected HttpError, but wasnt thrown");
+    }
+    catch (err: unknown) {
+      expect(err).toBeInstanceOf(HttpError);
+      const e = err as HttpError;
+      expect(e.status).toBe(404);
+      expect(e.translationCode).toBe("USER_NOT_FOUND");
+    }
   });
 
 })
